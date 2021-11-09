1e8a07f51bb79bffcdf69bf8a9cf4d617aa5a58e
Anthony Obaze <toneyobaze@gmail.com>
2021-11-09 11:21:31 +0000

    add scrollview, flatlist

--- App.js
+++ App.js
@@ -1,72 +1,96 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
   View,
-  TouchableOpacity,
-  Image,
+  Text,
   TextInput,
+  TouchableOpacity,
+  Button,
+  FlatList,
+  ScrollView,
 } from "react-native";
 
 function App() {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (text) => {
+    setEmail(text);
+  };
+
+  const handleSubmit = () => {
+    console.log("Clicked");
+  };
+
+  let students = [
+    "Prince",
+    "Mark",
+    "Tei",
+    "Maya",
+    "Randy",
+    "Bill",
+    "Tsotsoo",
+    "Isaac",
+    "Emmanuel",
+    "Frank",
+    "Orlando",
+    "Prince",
+    "Mark",
+    "Tei",
+    "Maya",
+    "Randy",
+    "Bill",
+    "Tsotsoo",
+    "Isaac",
+    "Emmanuel",
+    "Frank",
+    "Orlando",
+    "Prince",
+    "Mark",
+    "Tei",
+    "Maya",
+    "Randy",
+    "Bill",
+    "Tsotsoo",
+    "Isaac",
+    "Emmanuel",
+    "Frank",
+    "Orlando",
+  ];
+
   return (
-    <View style={styles.parent}>
-      <View style={styles.profileContainer}>
-        <Image source={require("./assets/1.png")} style={styles.profileImg} />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder={"Email"} />
-        <TextInput style={styles.textInput} />
-        <TextInput style={styles.textInput} />
-      </View>
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text>Submit</Text>
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container}>
+      {/* ScrollView */}
+      <ScrollView>
+        <View style={{ height: 30, backgroundColor: "red", width: 100 }}></View>
+      </ScrollView>
+      {/* Flatlist */}
+      <FlatList
+        data={students}
+        keyExtractor={(item, index) => {
+          return index;
+        }}
+        renderItem={(goat, index) => {
+          return (
+            <View
+              style={{
+                padding: 16,
+                backgroundColor: "tomato",
+                marginVertical: 2,
+              }}
+            >
+              <Text ke>{goat.item}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  parent: {
+  container: {
     flex: 1,
-  },
-
-  profileContainer: {
-    flex: 4.5,
-    alignItems: "center",
     justifyContent: "center",
-  },
-
-  inputContainer: {
-    flex: 4.5,
-    justifyContent: "center",
-  },
-
-  btnContainer: {
-    flex: 1,
-  },
-
-  profileImg: {
-    height: 100,
-    width: 100,
-    borderRadius: 50,
-  },
-
-  textInput: {
-    borderBottomWidth: 2,
-    borderBottomColor: "black",
-    padding: 16,
-  },
-
-  button: {
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    backgroundColor: "purple",
   },
 });
 
